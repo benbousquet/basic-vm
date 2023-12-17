d20c305b2e417ef6f6abb4a0794d1c813b9f4969
benbousquet <bbousque@asu.edu>
2023-12-17 21:44:25 +0900

    added new insturctions and tidied up old ones

--- src/main.c
+++ src/main.c
@@ -6,12 +6,26 @@ typedef enum {
   ADD,
   POP,
   SET,
+  GET,
+  MOV,
+  PRN,
+  GOT,
   HLT
 } InstructionSet;
+
+typedef enum {
+  A, B, C, REG_COUNT
+} Registers;
+
+int registers[REG_COUNT];
 
 const int program[] = {
   PSH, 5,
   PSH, 6,
+  SET, B, 8,
+  GET, B,
+  POP,
+  // GOT, 8,
   ADD,
   POP,
   HLT
@@ -26,26 +40,44 @@ int fetch() {
   return program[ip];
 }
 
+void addStack(int val) {
+  pstack_size++;
+  pstack = (int*)realloc(pstack, (pstack_size) * sizeof(int));
+  pstack[pstack_size - 1] = val;
+}
+
+int popStack() {
+  int top = pstack[pstack_size - 1];
+  pstack_size--;
+  pstack = (int*)realloc(pstack, (pstack_size) * sizeof(int));
+  return top;
+}
+
 void eval(int instruction) {
   int res;
   switch(instruction){
     case PSH:
-      pstack_size++;
-      pstack = (int*)realloc(pstack, (pstack_size) * sizeof(int));
-      pstack[pstack_size - 1] = program[ip];
+      addStack(program[ip]);
       ip++;
       break;
     case ADD:
-      res = pstack[pstack_size - 1] + pstack[pstack_size - 2];
-      pstack_size--;
-      pstack = (int*)realloc(pstack, (pstack_size) * sizeof(int));
-      pstack[pstack_size - 1] = res;
+      res = popStack() + popStack();
+      addStack(res);
       break;
     case POP:
-      printf("DEBUG: %d", pstack[pstack_size - 1]);
-      pstack_size--;
-      pstack = (int*)realloc(pstack, (pstack_size) * sizeof(int));
+      printf("DEBUG: %d\n", popStack());
       break;
+    case SET:
+      registers[program[ip]] = program[ip + 1];
+      ip += 2;
+      break;
+    case GET:
+      addStack(registers[program[ip]]);
+      ip++;
+      break;
+    // case GOT: broken rn ;(
+    //   ip = program[ip + 1] - 1;
+    //   break;
     case HLT:
       break;
     default:
